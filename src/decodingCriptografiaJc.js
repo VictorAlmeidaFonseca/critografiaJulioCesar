82f16dd1fc7a614a152d39adee8a60d8725eac0a
Victor Almeida <36797751+VictorAlmeidaFonseca@users.noreply.github.com>
2020-05-13 00:23:51 -0300

    Update decodingCriptografiaJc.js

--- src/decodingCriptografiaJc.js
+++ src/decodingCriptografiaJc.js
@@ -1,33 +1,17 @@
-
-
 function decoding(numero_casas, decifrado) {
-    
-    let solved = '';
-    
-    for ( let letter = 0; letter < decifrado.length ; letter++) {
-        
-        let index = decifrado[letter].charCodeAt();
-        
-        if ( index >= 65 && index <= 77) {
-        
-            solved += String.fromCharCode(index + numero_casas)
-        
-        } else if ( index >= 97 &&  index <= 122) {
-        
-            solved += String.fromCharCode(index - numero_casas)
-        
-        } else {
-        
-            solved += decifrado[letter];
+  let solved = '';
+  for ( let letter = 0; letter < decifrado.length ; letter++) {
+     let index = decifrado[letter].charCodeAt();
+      if ( index >= 65 && index <= 77) {
+        solved += String.fromCharCode(index + numero_casas)
+      } else if ( index >= 97 &&  index <= 122) {
+        solved += String.fromCharCode(index - numero_casas)
+      } else {
+        solved += decifrado[letter];
         }
     }
-
-    // console.log(solved);
-    return solved;
+return solved;
     
 };
 
 module.exports = decoding;
-
-
-
